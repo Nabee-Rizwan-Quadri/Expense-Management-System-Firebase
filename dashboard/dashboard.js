da6eda7e5401710d68161ea09e4943bdf3a287a5
Nabee-Rizwan-Quadri <80901728+Nabee-Rizwan-Quadri@users.noreply.github.com>
2021-07-10 11:24:30 +0500

    css version

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -3,46 +3,47 @@ var db = firebase.firestore();
 var uid
 
 firebase.auth().onAuthStateChanged((user) => {
-  if (!user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    location.href = '../auth-screens/login/login.html'
-    // ...
-  } else {
-    uid = user.uid
-    var docRef = db.collection("user").doc(uid);
+    if (!user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        location.href = '../auth-screens/login/login.html'
+            // ...
+    } else {
+        uid = user.uid
+        var docRef = db.collection("user").doc(uid);
 
-    docRef.get().then((doc) => {
-        if (doc.exists) {
-            console.log("Document data:", doc.data());
-            console.log("Document data:", doc.data().username);
-    
-            userName = doc.data().username
-            console.log(userName)
-    
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-    }).catch((error) => {
-        console.log("Error getting document:", error);
-    });
-    // User is signed out
-    // ...
-  }
+        docRef.get().then((doc) => {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+                console.log("Document data:", doc.data().username);
+
+                userName = doc.data().username
+                console.log(userName)
+                document.getElementById('userName').innerText = `Welcome ${userName}!`
+
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
+        // User is signed out
+        // ...
+    }
 });
 
 console.log(userName)
 
 let logout = () => {
-  firebase.auth().signOut().then(() => {
-  // Sign-out successful.
-  location.href = '../auth-screens/login/login.html'
-}).catch((error) => {
-  // An error happened.
-});
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        location.href = '../auth-screens/login/login.html'
+    }).catch((error) => {
+        // An error happened.
+    });
 }
 
 //firebase username on dashboard
-console.log(userName)
-document.getElementById('userName').innerText = `Welcome ${userName}!`
+// console.log(userName)
+// document.getElementById('userName').innerText = `Welcome ${userName}!`
